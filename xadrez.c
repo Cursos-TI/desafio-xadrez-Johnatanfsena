1eb180e66f521d3197d721c46c1c73df0865a6d6
Johnatanfsena <johnatan.sena@hotmail.com>
2025-04-21 23:06:58 +0000

    nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    int bispo = 0, torre = 0, rainha = 0;
-    printf("#### JOGO DE XADREX ####\n\n");
+    int bispo = 0, torre = 0, rainha = 0, cavalo = 0;
+    int casas =0;
+    int opcao;
 
+    printf("#### JOGO DE XADREX ####\n\n");
+    printf("Escolha uma peça para mover: \n");
+    printf("1. Bispo\n");
+    printf("2.Torre\n");
+    printf("3.Rainha\n");
+    printf("4.Cavalo\n");
+    printf("5. Sair\n");
+    scanf("%d", &opcao);
+
+    
+     if (opcao == 1){
     // Movimento do Bispo (5 vezes Cima + Direita)
      for ( bispo; bispo < 5; bispo++){
         printf("Bispo ande para cima\n");
         printf("Bispo agora ande para a direita\n");
         printf("\n");
      }
+     
+    }else if(opcao == 2){
     // Movimento da Torre (5 vezes para a  Direita)
         while (torre < 5){
             printf("Torre ande para a Direita\n");
             torre++;
             printf("\n");
         }
+    }else if(opcao == 3){
         // Movimento da Rainha (8 vezes para a Esquerda)
         do {
             printf("Rainha ande para a Esquerda\n");
@@ -23,6 +38,28 @@ int main() {
             printf("\n");
 
         } while(rainha < 8 );
+    
+    }else if(opcao == 4){
+    
+    // Movimento do Cavalo (Movimento em L: 2 para baixo e 1 para a esquerda)
+        for(cavalo = 1; cavalo <3; cavalo++){
+            casas = 0;
+        
+            while (casas < 2)
+            {
+               printf("Cavalo ande para baixo\n");
+               casas++;
+            }
+            printf("Uma casa para a esquerda\n");
+            printf("\n");
+            
+        }
+    }else if(opcao == 5){
+        printf("Saindo do jogo!\n");
+        break;
+     }else{
+        printf("Opção invalida, tente novamente.\n");
+     }
         
     return 0;
 }
